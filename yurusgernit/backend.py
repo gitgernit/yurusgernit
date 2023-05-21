53745776f8aa64b6f2ef032cac34321893d694c3
sahar04ek <gruzdevyp@mail.ru>
2023-05-21 14:52:54 +0300

    калькулятор с пробелами и без

--- yurusgernit/backend.py
+++ yurusgernit/backend.py
@@ -1,3 +1,29 @@
-print("Hello, github!")
-print("УДАЛЕНО!!!")
-print("юра тупой")
+a = input().split()
+answer= 0
+if(len(a)== 3):
+    if(a[1]=="+"):
+        answer = int(a[0])+int(a[2])
+    elif(a[1]=="-"):
+        answer = int(a[0])-int(a[2])
+    elif (a[1] == "*"):
+        answer = int(a[0])*int(a[2])
+    elif (a[1] == "/"):
+        answer = int(a[0])/int(a[2])
+elif(len(a)== 1):
+    if("+" in a[0]):
+        firstnumber= a[0][:a[0].index("+")]
+        sucondnumber= a[0][a[0].index("+")+1:]
+        answer= int(firstnumber) + int(sucondnumber)
+    elif ("-" in a[0]):
+        firstnumber = a[0][:a[0].index("-")]
+        sucondnumber = a[0][a[0].index("-") + 1:]
+        answer = int(firstnumber) - int(sucondnumber)
+    elif ("*" in a[0]):
+        firstnumber = a[0][:a[0].index("*")]
+        sucondnumber = a[0][a[0].index("*") + 1:]
+        answer = int(firstnumber) * int(sucondnumber)
+    elif ("/" in a[0]):
+        firstnumber = a[0][:a[0].index("/")]
+        sucondnumber = a[0][a[0].index("/") + 1:]
+        answer = int(firstnumber) / int(sucondnumber)
+print(answer)
